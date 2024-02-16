01a90b4dd7c3002573486fed8855c9b16c9eeae5
Jefferson Jales <jeffersonjales@hotmail.com>
2024-02-16 12:47:59 -0300

    Mais ajustes

--- components/skills.js
+++ components/skills.js
@@ -7,32 +7,31 @@ function __skill_add(skillName, skillPerc, color){
                 `<div class="progress-bars">` + 
                     '<div class="total-bar"></div>' +
                     `<div class="progress-bar" style="width: ${skillPerc}%; background-color: ${color}"></div>` +
-                    '<p class="progress-numbers" style="left: 0%;">0</p>' + 
-                    '<p class="progress-numbers" style="left: 100%;">10</p>' +
+                    '<p class="progress-numbers" style="left: 0.5%;">1</p>' + 
+                    '<p class="progress-numbers" style="left: 99%;">5</p>' +
                 '</div>' + 
             '</div>'+
         '</div>'
 }
 
 function add_skills(){
+    let colorRed = "rgba(255, 0, 0, 0.5)"
     let colorGreen = "rgba(0,255, 0, 0.5)"
-    let colorBlue = "rgba(255, 0, 0, 0.5)"
     let colorYellow = "rgba(255, 255, 0, 0.5)"
 
-    __skill_add("HTML5", 80, colorGreen);
-    __skill_add("CSS3", 70, colorGreen);
-    __skill_add("JavaScript", 100, colorGreen);
+    __skill_add("C#", 80, colorRed);
+    __skill_add("Unity 3D", 90, colorRed);
+    __skill_add("GML", 100, colorRed);
+    __skill_add("GameMaker", 100, colorRed);
+    __skill_add("Netcode", 20, colorRed);
 
-    __skill_add("Java", 60, colorBlue);
-    __skill_add("C# / .NET", 80, colorBlue);
-    __skill_add("Unity 3D", 90, colorBlue);
-    __skill_add("GML", 100, colorBlue);
-    __skill_add("GameMaker", 100, colorBlue);
-    __skill_add("Netcode", 20, colorBlue);
+    __skill_add("HTML5", 70, colorRed);
+    __skill_add("CSS3", 60, colorRed);
+    __skill_add("JavaScript", 100, colorRed);
 
-    __skill_add("SQL", 60, colorYellow);
-    __skill_add("API", 60, colorYellow);
-    __skill_add("Git", 90, colorYellow);
+    __skill_add("SQL", 60, colorRed);
+    __skill_add("API", 60, colorRed);
+    __skill_add("Git", 90, colorRed);
 }
 
 document.addEventListener("DOMContentLoaded", add_skills);
--- script/text.js
+++ script/text.js
@@ -11,19 +11,14 @@ class LanguageData{
 }
 
 var langDataArr = [];
-var langCurrent = LANGUAGES.PORTUGUESE;
 
 function add(id, arrkey){
     langDataArr.push(new LanguageData(id, arrkey));
 }
-add("langButton", [
-    "English",
-    "Português"
-])
 
 add("about1", [
     "Tenho 6 anos de experiência como programador e desenvolvedor de jogos digitais, com domínio em C#, Javascript, GML e Java. Sou formado em Jogos Digitais pela Estácio e Graduando em Sistemas e Mídias Digitais na Faculdade Federal do Ceará (UFC).",
-    "I have 6 years of experience as a programmer and game developer. com domínio em C#, Javascript, GML e Java. Sou formado em Jogos Digitais pela Estácio e Graduando em Sistemas e Mídias Digitais na Faculdade Federal do Ceará (UFC)."
+    "I have 6 years of experience as a programmer and digital game developer, proficient in C#, Javascript, GML, and Java. I graduated in Digital Games from Estácio and I'm currently pursuing a degree in Digital Systems and Media at the Federal University of Ceará (UFC)."
 ]);
 
 add("about2", [
@@ -31,23 +26,69 @@ add("about2", [
     'Atualmente estamos desenvolvendo o jogo <a href="https://store.steampowered.com/app/1829270/RoadOut/" target=”_blank” >RoadOut</a>,' + 
     'um jogo de ação e aventura, que será publicado em parceria com a <a href="https://dangenentertainment.com/">DANGEN Entertainment</a>.',
 
-    'Co-fundei, em 2018, a <a href="https://twitter.com/RoadoutGame" target=”_blank”>Rastrolabs Games Studio</a>, empresa focada em desenvolvimento de jogos digitais e uma das principais empresas do ramo no estado do Ceará.' + 
-    'Atualmente estamos desenvolvendo o jogo <a href="https://store.steampowered.com/app/1829270/RoadOut/" target=”_blank” >RoadOut</a>,' + 
-    'um jogo de ação e aventura, que será publicado em parceria com a <a href="https://dangenentertainment.com/">DANGEN Entertainment</a>.',
+    'I co-founded in 2018 the <a href="https://twitter.com/RoadoutGame" target="_blank">Rastrolabs Games Studio</a>,' +
+    'a company focused on digital game development and one of the leading companies in the field in the state of Ceará.' + 
+    'Currently, we are developing the game <a href="https://store.steampowered.com/app/1829270/RoadOut/" target="_blank">RoadOut</a>,' +
+    'an action-adventure game, which will be published in partnership with <a href="https://dangenentertainment.com/" target="_blank">DANGEN Entertainment</a>.'
 ]);
 
-function changeLanguage(){
+add("contatoInfo", [
+    "Para falar comigo, basta escrever para o e-mail abaixo ou entrar em contato por meio das minhas redes sociais.",
+    "To get in touch with me, simply write to the email below or reach out through my social media channels.",
+])
+
+add("contatoLabel", ["Contato", "Contact"]);
+add("projetoLabel", ["Projetos", "Projects"]);
+add("habilidadeLabel", ["Minhas Habilidades", "My Skills"])
+add("hardSkill1", [
+    "Tenho competências para trabalhar com desenvolvimento de Jogos Digitais com Unity (C#) e GameMaker (GML). Também tenho conhecimento em desenvolvimento full-stack e estou iniciando na área de Netcoding.", 
+    "I have skills to work with Digital Game Development using Unity (C#) and GameMaker (GML). I also have knowledge in full-stack development and I am starting in the area of Netcoding."
+])
+
+add("hardSkill2", [
+    "Abaixo você pode conferir minha afinidade com tecnologias e ferramentas, de pouco confortável (1) até bastente confortável (5).", 
+    "Below you can check my proficiency with technologies and tools, ranging from not comfortable (1) to very comfortable (5)."
+])
+
+add("project_roadout",[
+    "RoadOut é um jogo de ação e aventura desenvolvido no GameMaker com GML. Em produção desde 2021 com apoio da DANGEN Enterteiment. Lançamento previsto para 2024, para as plataformas PC, Nintendo Switch, XBOX Series X/S, Sony Playstation 5.",
+    "RoadOut is an action-adventure game developed in GameMaker with GML. In production since 2021 with support from DANGEN Entertainment. Scheduled for release in 2024 on PC, Nintendo Switch, XBOX Series X/S, and Sony Playstation 5."
+])
+add("project_capybara", [
+    "Capybara Hotel é um jogo no melhor estilo 'Onde está Wally?' desenvolvido no GameMaker com GML. Lançamento previsto para o segundo trimestre de 2024.",
+    "Capybara Hotel is a game in the style of 'Where's Waldo?' developed in GameMaker with GML. Scheduled for release in the second quarter of 2024."
+])
+add("project_pokedex", [
+    "Protótipo de POKéDEX utilizando a API de pokémon pokeapi.co, feito em Unity 3D com C#.",
+    "Prototype of a POKéDEX using the Pokémon API pokeapi.co, made in Unity 3D with C#."
+])
+
+add("created_by", [
+    "Criado Por Jefferson Jales",
+    "Created By Jefferson Jales"
+])
+add("created_when",[
+    "São Paulo - Brasil - 2024",
+    "São Paulo - Brazil - 2024"
+])
+
+function changeLanguage(lang_value){
     langDataArr.forEach(function(item){
         console.log(item);
-        let element = document.getElementById(item.id);
-        element.innerHTML = item.arrKeys[langCurrent];
+        const element = document.getElementById(item.id)
+        console.log(element);
+        
+        if(element != null)
+            element.innerHTML = item.arrKeys[lang_value];
     })
-
-    if(langCurrent == LANGUAGES.PORTUGUESE)
-        langCurrent = LANGUAGES.ENGLISH 
-    else 
-        langCurrent = LANGUAGES.PORTUGUESE;
 }
 
-document.addEventListener("DOMContentLoaded", changeLanguage);
+function setEnglishLanguage(){
+    changeLanguage(LANGUAGES.ENGLISH);
+}
 
+function setPortugeseLanguage(){
+    changeLanguage(LANGUAGES.PORTUGUESE);
+}
+
+document.addEventListener("DOMContentLoaded", setEnglishLanguage);
